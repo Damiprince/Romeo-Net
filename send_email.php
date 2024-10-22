39eaaa851d498b0cc1d4bf2422435b251bebc141
Damiprince <adamilare183@gmail.com>
2024-10-22 07:58:32 +0100

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "adamilare183@gmail.com";
+    $subject = "New Message from $name";
+    $body = "You have received a new message from $name.\n\nEmail: $email\n\nMessage: $message";
+
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
